5b97c276b8dd411853bfc9c6b0cc5049941fd0ec
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-03-09 19:30:17 -0600

    functional work

---
+++ go_seashore/sea.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = ""
+	}
+	fmt.Println(s)
+}
+
+func another() {
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = ""
+	}
+	fmt.Println(s)
+}
