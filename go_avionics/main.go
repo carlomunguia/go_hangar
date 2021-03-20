7423f68e6ef439a5b7a059f95ebb2d85602bdc51
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-03-20 10:36:04 -0500

    pointers/variables

---
+++ go_avionics/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+	p := new(int)
+	fmt.Println(*p)
+	*p = 2
+	fmt.Println(*p)
+}
