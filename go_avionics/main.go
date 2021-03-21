0881caf2584deb41546dd39756272a28d4d228a3
Carlo Munguia <carlo.munguia.82@gmail.com>
2021-03-21 18:26:03 -0500

    a focus on scoping

--- go_avionics/main.go
+++ go_avionics/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"strings"
 )
 
@@ -19,4 +20,40 @@ func main() {
 	fmt.Println(*p)
 	*p = 2
 	fmt.Println(*p)
+}
+
+func main2() {
+	myVar := "cat"
+	switch myVar {
+	case "cat":
+		log.Println("cat is set to cat")
+	}
+}
+
+var global *int
+
+func f() {
+	var x int
+	x = 1
+	global = &x
+}
+
+func g() {
+	y := new(int)
+	*y = 1
+}
+
+func gcd(x, y int) int {
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+func fib(n int) int {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		x, y = y, x+y
+	}
+	return x
 }
---
+++ go_avionics/popcount.go
@@ -0,0 +1,41 @@
+package popcount
+
+import "fmt"
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func hello() {
+	x := "hello!"
+	for i := 0; i < len(x); i++ {
+		x := x[i]
+		if x != '!' {
+			x := x + 'A' - 'a'
+			fmt.Printf("%c", x)
+		}
+	}
+
+	if x := f(); x == 0 {
+		fmt.Println(x)
+	} else if y := g(x); x == y {
+		fmt.Println(x, y)
+	} else {
+		fmt.Println(x, y)
+	}
+}
