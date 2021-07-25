7b4359391cd1bc57a387d9f6b02d68426a46b1f5
Carlo Munguia <cmunguia@denimgroup.com>
2021-07-25 12:00:01 -0500

    range fundamentals / recap

--- go_aileron/main.go
+++ go_aileron/main.go
@@ -3,11 +3,20 @@ package main
 import "log"
 
 func main() {
-	animals := make(map[string]string)
-	animals["dog"] = "Fido"
-	animals["cat"] = "Fluffy"
+	type User struct {
+		FirstName string
+		LastName  string
+		Email     string
+		Age       int
+	}
 
-	for animalType, animal := range animals {
-		log.Println(animalType, animal)
+	var users []User
+	users = append(users, User{"John", "Smith", "john@smith.com", 30})
+	users = append(users, User{"Carlo", "Smith", "carlo@smith.com", 32})
+	users = append(users, User{"Leon", "Smith", "leon@smith.com", 22})
+	users = append(users, User{"Paco", "Smith", "paco@smith.com", 29})
+
+	for _, l := range users {
+		log.Println(l.FirstName, l.LastName, l.Age, l.Email)
 	}
 }
