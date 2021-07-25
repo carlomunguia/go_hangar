da8cea02782c4f0cd1c33e4f7a348f306a8e43d4
Carlo Munguia <cmunguia@denimgroup.com>
2021-07-25 10:46:32 -0500

    small incremental commits to reinforce already learned fundamentals for work on new project outside

--- go_aileron/main.go
+++ go_aileron/main.go
@@ -1,43 +1,13 @@
 package main
 
-import (
-	"log"
-	"sort"
-)
+import "log"
 
 func main() {
-	var miSlice []string
+	animals := make(map[string]string)
+	animals["dog"] = "Fido"
+	animals["cat"] = "Fluffy"
 
-	miSlice = append(miSlice, "Pink")
-	miSlice = append(miSlice, "Moon")
-
-	log.Println(miSlice)
-
-	numbers := []int{9, 1, 3, 6, 2, 3, 1, 3, 2}
-	sort.Ints(numbers)
-	log.Println(numbers)
-
-	var isTrue bool
-	var isFalse bool
-
-	isTrue = true
-	isFalse = false
-
-	if isTrue {
-		log.Println("isTrue is", isTrue)
-	} else {
-		log.Println("isFalse is", isFalse)
-	}
-
-	cat := "cat"
-
-	if cat == "cat" {
-		log.Println("meow meow")
-	}
-
-	myNum := 100
-
-	if myNum > 99 && isTrue {
-		log.Println("totally normal conditional stuff")
+	for animalType, animal := range animals {
+		log.Println(animalType, animal)
 	}
 }
