fc6794608dc6e05fc37210287a248bfd07984714
Carlo Munguia <cmunguia@denimgroup.com>
2021-07-25 16:07:07 -0500

    interface refresher

--- go_aileron/main.go
+++ go_aileron/main.go
@@ -1,22 +1,55 @@
 package main
 
-import "log"
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-	type User struct {
-		FirstName string
-		LastName  string
-		Email     string
-		Age       int
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "Shih Tzu",
+	}
+	PrintInfo(&dog)
+
+	gorilla := Gorilla{
+		Name:          "Jacques",
+		Color:         "Silverback",
+		NumberOfTeeth: 38,
 	}
 
-	var users []User
-	users = append(users, User{"John", "Smith", "john@smith.com", 30})
-	users = append(users, User{"Carlo", "Smith", "carlo@smith.com", 32})
-	users = append(users, User{"Leon", "Smith", "leon@smith.com", 22})
-	users = append(users, User{"Paco", "Smith", "paco@smith.com", 29})
+	PrintInfo(&gorilla)
+}
 
-	for _, l := range users {
-		log.Println(l.FirstName, l.LastName, l.Age, l.Email)
-	}
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (g *Gorilla) Says() string {
+	return "Roar"
+}
+
+func (g *Gorilla) NumberOfLegs() int {
+	return 2
 }
