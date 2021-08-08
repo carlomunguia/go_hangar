9989ed99531b3493aacea68e7fa997180d2246de
Carlo Munguia <cmunguia@denimgroup.com>
2021-08-07 23:33:13 -0500

    reviewing test procedures

--- go_aileron/main.go
+++ go_aileron/main.go
@@ -1,69 +1,26 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
 	"log"
 )
 
-type Person struct {
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	HairColor string `json:"hair_color"`
-	HasDog    bool   `json:"has_dog"`
+func main() {
+	result, err := divide(100.0, 0)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Println("result of division is", result)
 }
 
-func main() {
-	myJson := `
-		[
-			{
-				"first_name": "Clark",
-				"last_name": "Kent",
-				"hair_color": "black",
-				"has_dog": true
-			},
-			{
-				"first_name": "Bruce",
-				"last_name": "Wayne",
-				"hair_color": "black",
-				"has_dog": false
-			}
+func divide(x, y float32) (float32, error) {
+	var result float32
 
-]`
-
-	var unmarshalled []Person
-
-	err := json.Unmarshal([]byte(myJson), &unmarshalled)
-	if err != nil {
-		log.Println("error unmarshalling json", err)
+	if y == 0 {
+		return result, errors.New("Cannot divide by Zero")
 	}
-
-	log.Printf("unmarshalled: %v", unmarshalled)
-
-	var mySlice []Person
-
-	var m1 Person
-
-	m1.FirstName = "Wally"
-	m1.LastName = "West"
-	m1.HairColor = "Red"
-	m1.HasDog = false
-
-	mySlice = append(mySlice, m1)
-
-	var m2 Person
-
-	m2.FirstName = "Dale"
-	m2.LastName = "Cooper"
-	m2.HairColor = "Black"
-	m2.HasDog = false
-
-	mySlice = append(mySlice, m2)
-
-	newJson, err := json.MarshalIndent(mySlice, "", "   ")
-	if err != nil {
-		log.Println("Error marshalling json", err)
-	}
-
-	fmt.Println(string(newJson))
+	result = x / y
+	return result, nil
 }
