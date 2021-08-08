d38d97fbf4e57538132f8ea15c6774f190581f61
Carlo Munguia <cmunguia@denimgroup.com>
2021-08-07 23:05:20 -0500

    json marshalling/unmarshalling refresher

--- go_aileron/main.go
+++ go_aileron/main.go
@@ -1,30 +1,69 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
-	"math/rand"
-	"time"
 )
 
-const numPool = 10
-
-func CalculateValue(intChan chan int) {
-	randomNumber := RandomNumber(numPool)
-	intChan <- randomNumber
+type Person struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	HairColor string `json:"hair_color"`
+	HasDog    bool   `json:"has_dog"`
 }
 
 func main() {
-	intChan := make(chan int)
-	defer close(intChan)
+	myJson := `
+		[
+			{
+				"first_name": "Clark",
+				"last_name": "Kent",
+				"hair_color": "black",
+				"has_dog": true
+			},
+			{
+				"first_name": "Bruce",
+				"last_name": "Wayne",
+				"hair_color": "black",
+				"has_dog": false
+			}
 
-	go CalculateValue(intChan)
+]`
 
-	num := <-intChan
-	log.Println(num)
-}
+	var unmarshalled []Person
 
-func RandomNumber(n int) int {
-	rand.Seed(time.Now().UnixNano())
-	value := rand.Intn(n)
-	return value
+	err := json.Unmarshal([]byte(myJson), &unmarshalled)
+	if err != nil {
+		log.Println("error unmarshalling json", err)
+	}
+
+	log.Printf("unmarshalled: %v", unmarshalled)
+
+	var mySlice []Person
+
+	var m1 Person
+
+	m1.FirstName = "Wally"
+	m1.LastName = "West"
+	m1.HairColor = "Red"
+	m1.HasDog = false
+
+	mySlice = append(mySlice, m1)
+
+	var m2 Person
+
+	m2.FirstName = "Dale"
+	m2.LastName = "Cooper"
+	m2.HairColor = "Black"
+	m2.HasDog = false
+
+	mySlice = append(mySlice, m2)
+
+	newJson, err := json.MarshalIndent(mySlice, "", "   ")
+	if err != nil {
+		log.Println("Error marshalling json", err)
+	}
+
+	fmt.Println(string(newJson))
 }
